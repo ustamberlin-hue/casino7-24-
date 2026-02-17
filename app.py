dbb54ad6cc04c04ac2035234b3561a102b73243d
ustamberlin-hue <ustamberlin@gmail.com>
2026-02-17 11:20:20 +0100

    app.py

--- app.py
+++ app.py
@@ -196,5 +196,7 @@ def logout():
     session.clear()
     return redirect('/')
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 9090)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
