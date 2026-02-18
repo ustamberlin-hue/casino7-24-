138fa84fe8b06a712fb54d3545f148697cec6dd0
ustamberlin-hue <ustamberlin@gmail.com>
2026-02-18 18:45:39 +0100

    app.py

--- app.py
+++ app.py
@@ -6,34 +6,30 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'gizli-anahtar'
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
-# Aktif kullanıcıları saklamak için sözlük
+# Aktif kullanıcıları saklamak için
 users = {}
 
 @app.route('/')
 def index():
+    # templates/index.html dosyasını arar
     return render_template('index.html')
 
 @socketio.on('join')
 def handle_join(data):
-    username = data['username']
+    username = data.get('username', 'Misafir')
     users[request.sid] = username
     join_room('chat_room')
-    # Yeni listeyi herkese gönder
     emit('user_list', list(users.values()), room='chat_room')
-    print(f"{username} katıldı.")
 
 @socketio.on('disconnect')
 def handle_disconnect():
     if request.sid in users:
-        username = users[request.sid]
         del users[request.sid]
-        # Güncel listeyi herkese gönder (çıkan kişi silinmiş olur)
         emit('user_list', list(users.values()), room='chat_room')
-        print(f"{username} ayrıldı.")
 
 @socketio.on('signal')
 def handle_signal(data):
-    # Arama sinyallerini (offer, answer, candidate) hedefe ilet
+    # Görüntü sinyallerini karşı tarafa iletir
     emit('signal', data, broadcast=True, include_self=False)
 
 if __name__ == '__main__':
