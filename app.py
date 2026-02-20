18ff1c3d9f6aaf8648a990879076d6ba26e5b964
ustamberlin-hue <ustamberlin@gmail.com>
2026-02-20 21:26:05 +0100

    Update app.run to use dynamic port from environment
    Modify app.run to use environment variable for port

--- app.py
+++ app.py
@@ -64,4 +64,7 @@ HTML_SABLONU = """
 """
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    # Render'ın verdiği portu kullanması için bu şart:
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
